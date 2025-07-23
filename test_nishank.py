d0611b12e257893f69fd2c64a243aaad40fe8e08
Nishank Narnaware <nishanknarnaware@gmail.com>
2025-07-23 12:53:23 +0900

    Test sample

---
+++ test_nishank.py
@@ -0,0 +1,16 @@
+print("Hello")
+a = 100
+print(a*2)
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
